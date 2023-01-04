3380141700d98382417ac407c76503972ee24db1
aqsafaisal34 <aqsay.faisal34@gmail.com>
2023-01-04 17:22:50 +0500

    create data

---
+++ src/components/Create.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+
+function Create() {
+   const [name,setName] = useState('');
+   const [age,setAge] = useState('');
+   const [email,setEmail] = useState('');
+
+   const submitHandler = (e) =>{
+    e.preventDefault();
+    axios.post('https://63b5680958084a7af3933503.mockapi.io/crud',{
+        e_name: name,
+        e_age: age,
+        e_email: email
+    })
+   }
+   
+  return (
+    <>
+      <div className='row'>
+        <div className='col-md-4'>
+        <div className='bg-primary p-2 text-center'>
+            <h1>Create Data</h1>
+        </div>
+         <form onSubmit={submitHandler}>
+            <div className='form-group'>
+                <label>Enter Name:</label>
+                <input type='text' placeholder='Name' className='form-control' onChange={(e)=>setName(e.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Enter Age:</label>
+                <input type='number' placeholder='Age' className='form-control' onChange={(e)=>setAge(e.target.value)}/>
+            </div>
+            <div className='form-group'>
+                <label>Enter Email:</label>
+                <input type='email' placeholder='Email' className='form-control'onChange={(e)=>setEmail(e.target.value)}/>
+            </div>
+            <br />
+            <div className='d-grid'>
+            <input type='submit' value='Submit' className='btn btn-primary'/>
+            </div>
+         </form>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Create;
